e14c145e838c3291f94af6a49b93eb3ebdfc13ba
dvargas42 <dvargas.eng@gmail.com>
2021-06-09 18:14:00 -0300

    feat: include dashboard shield

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,45 +1,49 @@
 import { useMovements } from '../../hooks/useMovements'
 
-import roverImg from '../../assets/profile.jpg'
+import roverImg from '../../assets/profile.png'
 import '../../styles/dashboard.scss'
 
 export function Dashboard() {
   const { roversData } = useMovements()
+
   return (
     <main className="dashContainer">
       <div className="dashContent">
-        <ul className="cardContainer">
-          {roversData.map(item => (
-            <li className="cardContent"
-              key={ item.roverName }
-            >
-              <img src={roverImg} alt="Avatar" />
+        { (roversData.length !== 0) ? (
+          <ul className="cardContainer">
+            {roversData.map(item => (
+              <li className="cardContent"
+                key={ item.roverName }
+              >
+                <img src={roverImg} alt="Avatar" />
 
-              <ul>
-                <li>
-                  <p>Name:</p>
-                  {item.roverName}
-                </li>
+                <ul>
+                  <li>
+                    <p>Name:</p>
+                    {item.roverName}
+                  </li>
 
-                <li className="right">
-                  <p>Posição inicial:</p>
-                  {`${item.xInitialPosition} ${item.yInitialPosition} ${item.initialDirection}`}
-                </li>
+                  <li className="right">
+                    <p>Posição inicial:</p>
+                    {`${item.xInitialPosition} ${item.yInitialPosition} ${item.initialDirection}`}
+                  </li>
 
-                <li>
-                  <p>Instrução:</p>
-                  {item.instruction}
-                </li>
+                  <li>
+                    <p>Instrução:</p>
+                    {item.instruction}
+                  </li>
 
-                <li className="right">
-                  <p>Posição final:</p>
-                  {`${item.xFinalPosition} ${item.yFinalPosition} ${item.finalDirection}`}
-                </li>
-            </ul>
-              
-          </li>
-          ))}
-        </ul>
+                  <li className="right">
+                    <p>Posição final:</p>
+                    {`${item.xFinalPosition} ${item.yFinalPosition} ${item.finalDirection}`}
+                  </li>
+                </ul>
+              </li>
+            ))}
+          </ul>
+        ):(
+          <img className="heroImg" src={roverImg} alt="hero"></img>
+        )}
     </div>
     </main>
   )
